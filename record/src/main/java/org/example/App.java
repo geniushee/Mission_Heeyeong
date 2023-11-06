a9bb9a4ae0ef133c78eb2dddd452822a082c8fff
geniushee <h1116jeon@gmail.com>
2023-11-06 10:34:09 +0900

    1,2step complete

---
+++ record/src/main/java/org/example/App.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    static Scanner scanner; // 표준입력 클래스
+    static boolean running; // 스위치
+
+    List<Quotation> quotations; // 명언 저장소
+
+    // 초기화
+    App(){
+        scanner = new Scanner(System.in);
+        running = true;
+        quotations = new ArrayList<>();
+    }
+
+    public void run(){
+        String cmd; // 명령어 받을 객체
+
+        System.out.println("== 명언 앱 ==");
+
+        //명언앱 구동스위치
+        while(running){
+            System.out.print("명령)");
+            cmd = scanner.nextLine();
+            // 종료 조건
+            if(cmd.equals("종료")){
+                exitApp();
+
+            } else if (cmd.equals("등록")) {
+                enroll(); // 등록 기능
+            }
+        }
+
+
+    }
+
+    void exitApp(){
+        running = false;
+    }
+
+    void enroll(){
+        System.out.print("명언 : "); // 명언 입력
+        String content = scanner.nextLine();
+        System.out.print("작가 : "); // 작가 입력
+        String author = scanner.nextLine();
+
+        Quotation q = new Quotation(1, content, author); // id, 명언, 작가 객체에 저장
+        quotations.add(q);  // 저장소에 저장
+    }
+}
---
+++ record/src/main/java/org/example/Quotation.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Quotation {
+    int id;
+    String content;
+    String author;
+
+    Quotation(int id, String content, String author){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
